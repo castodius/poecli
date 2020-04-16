014b74031ba492abf53254c475cd07f533212785
Urban Pettersson <urbanpe88@gmail.com>
2020-04-16 17:18:05 +0200

    chore: write code for listing projects

---
+++ src/actions/projects/list.ts
@@ -0,0 +1,9 @@
+import { POEditor } from '@lib/poeditor';
+import *  as log from '@lib/log';
+
+export const list = async (): Promise<void> => {
+  const poe = new POEditor();
+
+  const data = await poe.listProjects();
+  log.info(JSON.stringify(data));
+}
