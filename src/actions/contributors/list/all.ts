c08f7f53d123350891d6b7499dc3110b103a2c7d
Urban Pettersson <urbanpe88@gmail.com>
2020-04-26 07:41:42 +0200

    chore: write action for listing all contributors

---
+++ src/actions/contributors/list/all.ts
@@ -0,0 +1,11 @@
+import { POEditor } from '@lib/poeditor'
+import { Contributor } from '@models/poeditor'
+import * as log from '@lib/log'
+
+export const all = async (): Promise<void> => {
+  const poe = new POEditor()
+
+  const contributors: Contributor[] = await poe.listContributors({})
+
+  log.info(JSON.stringify(contributors))
+}
