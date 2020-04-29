d4a903c7f55550b278b625d9438d7fcefb2ccb6f
Urban Pettersson <urbanpe88@gmail.com>
2020-04-29 17:06:34 +0200

    chore: write generic mapping function for inquirer display

--- src/helpers/prompt.ts
+++ src/helpers/prompt.ts
@@ -14,3 +14,24 @@ export const getConfirmation = async (message: string): Promise<boolean> => {
 
   return confirm
 }
+
+export interface Choice<T> {
+  name: string;
+  value: T;
+}
+
+/**
+ * Takes an array of T and returns a ready to use array of Choice for inquirer purposes
+ * @param items
+ * Items to be mapped
+ * @param nameMapper
+ * Function which can get a name from a T
+ */
+export const mapToChoices = <T>(items: T[], nameMapper: (item: T) => string): Choice<T>[] => {
+  return items.map((item: T): Choice<T> => {
+    return {
+      name: nameMapper(item),
+      value: item
+    }
+  })
+}
