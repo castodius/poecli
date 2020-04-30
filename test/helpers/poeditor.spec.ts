605e08c2a5a713a5251c3110bd3841be20330bd4
Urban Pettersson <urbanpe88@gmail.com>
2020-04-30 19:34:56 +0200

    chore: write unit tests for selectLanguage and filterLanguages

--- test/helpers/poeditor.spec.ts
+++ test/helpers/poeditor.spec.ts
@@ -4,7 +4,7 @@ import * as poeditorHelper from '@helpers/poeditor'
 // tools
 import { stdin, MockSTDIN } from 'mock-stdin'
 import { POEditor } from '@lib/poeditor'
-import { CompactProject } from '@models/poeditor'
+import { CompactProject, Language, ProjectLanguage } from '@models/poeditor'
 
 // models
 
@@ -30,7 +30,6 @@ describe('poeditor', () => {
       process.nextTick(() => {
         stdinMock.send('\r')
       })
-
       const output = await poeditorHelper.selectProject(poe)
 
       expect(output).toEqual(projects[0])
@@ -43,6 +42,97 @@ describe('poeditor', () => {
       const output = await poeditorHelper.selectProject(poe)
 
       expect(output).toEqual(undefined)
+    })
+  })
+
+  describe('selectProject', () => {
+    const languages: Language[] = [{
+      name: 'Swenglish',
+      code: 'sv'
+    }]
+
+    it('should select a language', async () => {
+      const poe = new POEditor('abc')
+      jest.spyOn(poe, 'getAvailableLanguages').mockResolvedValue(languages)
+      const excludeSpy = jest.spyOn(poeditorHelper, 'filterLanguages')
+
+      process.nextTick(() => {
+        stdinMock.send('\r')
+      })
+      const output = await poeditorHelper.selectLanguage(poe)
+
+      expect(output).toEqual(languages[0])
+      expect(excludeSpy.mock.calls.length).toEqual(0)
+      excludeSpy.mockRestore()
+    })
+
+    it('should call exclude when exclude list is defined', async () => {
+      const poe = new POEditor('abc')
+      jest.spyOn(poe, 'getAvailableLanguages').mockResolvedValue(languages)
+      const excludeSpy = jest.spyOn(poeditorHelper, 'filterLanguages').mockReturnValue(languages)
+
+      process.nextTick(() => {
+        stdinMock.send('\r')
+      })
+      const output = await poeditorHelper.selectLanguage(poe, [])
+
+      expect(output).toEqual(languages[0])
+      expect(excludeSpy.mock.calls.length).toEqual(1)
+      excludeSpy.mockRestore()
+    })
+
+    it('should return nothing when no languages are available', async () => {
+      const poe = new POEditor('abc')
+      jest.spyOn(poe, 'getAvailableLanguages').mockResolvedValue([])
+      const excludeSpy = jest.spyOn(poeditorHelper, 'filterLanguages')
+
+      process.nextTick(() => {
+        stdinMock.send('\r')
+      })
+      const output = await poeditorHelper.selectLanguage(poe)
+
+      expect(output).toEqual(undefined)
+      expect(excludeSpy.mock.calls.length).toEqual(0)
+      excludeSpy.mockRestore()
+    })
+  })
+
+  describe('filterLanguages', () => {
+    it('should filter languages', () => {
+      const languages: Language[] = [{
+        name: 'Swedish',
+        code: 'sv'
+      }, {
+        name: 'English',
+        code: 'en'
+      }, {
+        name: 'Finnish',
+        code: 'fi'
+      }]
+
+      const filters: ProjectLanguage[] = [{
+        name: 'Danish',
+        code: 'da',
+        translations: 0,
+        percentage: 0,
+        updated: ''
+      }, {
+        name: 'English',
+        code: 'en',
+        translations: 0,
+        percentage: 0,
+        updated: ''
+      }]
+
+      const output = poeditorHelper.filterLanguages(languages, filters)
+
+      expect(output).toEqual([{
+        name: 'Swedish',
+        code: 'sv'
+      }, {
+        name: 'Finnish',
+        code: 'fi'
+      }])
     })
   })
 })
