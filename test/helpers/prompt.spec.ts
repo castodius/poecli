cdc7d0bab3c11eec56e7a423387015857ac292cf
Urban Pettersson <urbanpe88@gmail.com>
2020-05-03 19:52:40 +0200

    chore: finish up unit tests for prompt (questionable tests)

--- test/helpers/prompt.spec.ts
+++ test/helpers/prompt.spec.ts
@@ -55,4 +55,105 @@ describe('prompt', () => {
       }])
     })
   })
+
+  describe('selectX', () => {
+    it('should be able to select an item from a list', async () => {
+      process.nextTick(() => {
+        stdinMock.send('\r')
+      })
+      const choices = promptHelper.mapToChoices<string>(['abc', 'def'], (item: string) => item)
+
+      const output = await promptHelper.selectX(choices, 'some text')
+
+      expect(output).toEqual('abc')
+    })
+  })
+
+  describe('selectAuto', () => {
+    /**
+     * Yes, I wanted to test d followed by carriage return here. Could not get it to work
+     */
+    it('should be able to select an item using autocomplete', async () => {
+      process.nextTick(() => {
+        stdinMock.send('\r')
+      })
+
+      const output = await promptHelper.selectAuto<string>('some text', promptHelper.buildStringSourceFunction(['abc', 'def']))
+
+      expect(output).toEqual('abc')
+    })
+  })
+
+  describe('selectCheckboxPlus', () => {
+    it('should be able to select an item using checkbox plus', async () => {
+      process.nextTick(() => {
+        stdinMock.send(' \r')
+      })
+
+      const output = await promptHelper.selectCheckboxPlus<string>('some text', promptHelper.buildStringSourceFunction(['abc', 'def']))
+
+      expect(output).toEqual(['abc'])
+    })
+  })
+
+  describe('promptInput', () => {
+    it('should return input from user', async () => {
+      process.nextTick(() => {
+        stdinMock.send('abc')
+        stdinMock.send('\r')
+      })
+
+      const output = await promptHelper.promptInput('write something')
+
+      expect(output).toEqual('abc')
+    })
+
+    it('should use default', async () => {
+      process.nextTick(() => {
+        stdinMock.send('\r')
+      })
+
+      const output = await promptHelper.promptInput('write something', 'so basic')
+
+      expect(output).toEqual('so basic')
+    })
+  })
+
+  describe('buildStringSourceFunction', () => {
+    it('should return a source function', async () => {
+      const choices = ['abc', 'def', 'ad']
+      const foo = promptHelper.buildStringSourceFunction(choices)
+
+      const all = await foo('', '')
+      expect(all).toEqual(choices)
+
+      const onlyA = await foo('', 'a')
+      expect(onlyA).toEqual(['abc', 'ad'])
+
+      const onlyD = await foo('', 'd')
+      expect(onlyD).toEqual(['def', 'ad'])
+
+      const nothing = await foo('', 'banana')
+      expect(nothing).toEqual([])
+    })
+  })
+
+  describe('buildChoiceSourceFunction', () => {
+    it('should return a source function', async () => {
+      const choices = promptHelper.mapToChoices<string>(['abc', 'def', 'ad'], item => item)
+      const foo = promptHelper.buildChoiceSourceFunction(choices)
+
+      const all = await foo('', '')
+      expect(all).toEqual(choices)
+
+      const onlyA = await foo('', 'a')
+      expect(onlyA).toEqual([choices[0], choices[2]])
+
+      const onlyD = await foo('', 'd')
+      expect(onlyD).toEqual([choices[1], choices[2]])
+
+      const nothing = await foo('', 'banana')
+      expect(nothing).toEqual([])
+    })
+  })
 })
